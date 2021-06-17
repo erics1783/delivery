a334a017c64f3d740dd19510a624661176be3286
Eric <erics1783@gmail.com>
2021-06-17 12:43:15 +0000

    pushing a build trigger

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask
+import logging
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World.'
+    return 'Hello World!'
 
-if __name__=='__main__':
+@app.route('/newroute/<name>')
+def newroute(name):
+    """parameter"""
+    return "this was passed in: %s" % name
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
